945f7c3b7afa088a68af535512b1ed5d3d125c5a
iqbalpalemad <iqbal@nethram.com>
2021-07-31 12:46:51 +0530

    Login API added

---
+++ api_app/Controllers/auth/login.js
@@ -0,0 +1,27 @@
+const grpcClient                = require('../../grpcClient/grpcUserClient');
+const { validationResult }      = require('express-validator')
+
+const login = async (req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({result : false, errors: errors.array() })
+    }
+    try{
+        const userRequest = {
+            email    : req.body.email,
+            password : req.body.password
+        }
+        grpcClient.login(userRequest, (err, response) => {
+            if(err){
+                res.status(500).json({result : false, error : err.message});
+            }
+
+            res.status(201).json(response);
+        })
+    }
+    catch(err){
+        res.status(500).json({result : false, error : err.message});
+    }
+}
+
+module.exports = login;
