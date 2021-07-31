66ce90fa04ecfea18a02c35208f2f1b435cda1aa
iqbalpalemad <iqbal@nethram.com>
2021-07-31 12:29:13 +0530

    duplicate email check added in signup

--- grp_service/controller/user/createUser.js
+++ grp_service/controller/user/createUser.js
@@ -7,25 +7,29 @@ const User                      = require('../../Model/User')
 const createUser = async (call,callback) => {
 
     try{
+        let userResponse = {};
+        const existingUser = await User.findOne({email:call.request.email});
+        if(existingUser){
+            userResponse.result  = false,
+            userResponse.message = "Email address already taken"
+            return callback(null,userResponse);
+        }
         const passwordHash = await bcrypt.hash(call.request.password,12);
         const user         = new User({
                 email    : call.request.email,
                 password : passwordHash
         });
         const save = await user.save();
-        const userResponse = {
-            result  : true,
-            message : save._id
-        }
-        callback(null,userResponse);
+        userResponse.result  = true,
+        userResponse.message = save._id
+        return callback(null,userResponse);
     }
     catch(err){
-        console.log(err)
         const userResponse = {
             result  : false,
             message : err.message
         }
-        callback(null,userResponse);
+        return callback(null,userResponse);
     }
 }
 
